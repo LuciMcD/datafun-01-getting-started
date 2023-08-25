4236391c79696018d358d33d695f5db93b3076fe
LuciMcD <lucimcdaniel@gmail.com>
2023-08-24 19:40:10 -0500

    first todo in acquainted

--- acquainted.py
+++ acquainted.py
@@ -48,21 +48,21 @@ logger, logname = setup_logger(__file__)
 # TODO: Customize these by changing the values - use your own name, etc.
 
 # String data types
-name = "John Doe"
-state = "California"
+name = "Luci McDaniel"
+state = "Missouri"
 country = "USA"
 
 # Integer data types
-pet_count = 1
-skill_count = 24
+pet_count = 2
+skill_count = 42
 
 # Float data types (floating point numbers, with a decimal point)
-temperature_f = 81.5
-pet_weight_lbs = 12.5
+temperature_f = 92.9
+pet_weight_lbs = 260.6
 
 
 # Boolean data types (True or False)
-has_dog = False
+has_dog = True
 likes_analytics = True
 likes_python = True
 
